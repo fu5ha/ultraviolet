6f512632e7b055bc612a46fc72fc2c6e031b3957
Gray Olson <gray@grayolson.com>
2019-10-30 22:29:35 -0700

    add refraction, vec4, mat4

--- src/mat.rs
+++ src/mat.rs
@@ -36,3 +36,40 @@ macro_rules! mat3s {
 }
 
 mat3s!(Mat3 => Vec3, Wat3 => Wec3);
+
+macro_rules! mat4s {
+    ($($n:ident => $t:ident),+) => {
+        $(#[derive(Clone, Copy, Debug)]
+        pub struct $n {
+            pub cols: [$t; 4],
+        }
+
+        impl $n {
+            #[inline]
+            pub fn new(col1: $t, col2: $t, col3: $t, col4: $t) -> Self {
+                $n {
+                    cols: [col1, col2, col3, col4],
+                }
+            }
+        }
+
+        impl Mul<$t> for $n {
+            type Output = $t;
+            #[inline]
+            fn mul(self, rhs: $t) -> $t {
+                let a = self.cols[0];
+                let b = self.cols[1];
+                let c = self.cols[2];
+                let d = self.cols[3];
+                $t::new(
+                    a.x * rhs.x + b.x * rhs.y + c.x * rhs.z + d.x * rhs.w,
+                    a.y * rhs.x + b.y * rhs.y + c.y * rhs.z + d.y * rhs.w,
+                    a.z * rhs.x + b.z * rhs.y + c.z * rhs.z + d.z * rhs.w,
+                    a.w * rhs.x + b.w * rhs.y + c.w * rhs.z + d.z * rhs.w,
+                )
+            }
+        })+
+    }
+}
+
+mat4s!(Mat4 => Vec4, Wat4 => Wec4);
--- src/vec.rs
+++ src/vec.rs
@@ -27,16 +27,10 @@ macro_rules! vec2s {
             }
 
             #[inline]
-            pub fn reflect(&mut self, normal: $n) {
-                *self = *self - ($t::from(2.0) * self.dot(normal) * normal);
+            pub fn reflected(&self, normal: $n) -> Self {
+                *self - ($t::from(2.0) * self.dot(normal) * normal)
             }
 
-            #[inline]
-            pub fn reflected(&self, normal: $n) -> Self {
-                let mut a = *self;
-                a.reflect(normal);
-                a
-            }
 
             #[inline]
             pub fn mag_sq(&self) -> $t {
@@ -235,6 +229,26 @@ impl From<[Vec2; 4]> for Wec2 {
     }
 }
 
+impl Vec2 {
+    #[inline]
+    pub fn refracted(&mut self, normal: Self, eta: f32) -> Self {
+        let n = normal;
+        let i = *self;
+        let ndi = n.dot(i);
+        let k = 1.0 - eta * eta * (1.0 - ndi * ndi);
+        if k < 0.0 {
+            Self::zero()
+        } else {
+            i * eta - n * (eta * ndi * k.sqrt())
+        }
+    }
+
+    #[inline]
+    pub fn into_homogeneous(self) -> Vec3 {
+        Vec3::new(self.x, self.y, 1.0)
+    }
+}
+
 impl Wec2 {
     #[inline]
     pub fn splat(vec: Vec2) -> Self {
@@ -247,6 +261,26 @@ impl Wec2 {
             x: f32x4::merge(mask, a.x, b.x),
             y: f32x4::merge(mask, a.y, b.y),
         }
+    }
+
+    #[inline]
+    pub fn refracted(&mut self, normal: Self, eta: f32x4) -> Self {
+        let n = normal;
+        let i = *self;
+        let one = f32x4::from(1.0);
+        let ndi = n.dot(i);
+
+        let k = one - eta * eta * (one - ndi * ndi);
+        let mask = k.cmp_lt(f32x4::from(0.0));
+
+        let out = i * eta - n * (eta * ndi * k.sqrt());
+
+        Self::merge(mask, out, Self::zero())
+    }
+
+    #[inline]
+    pub fn into_homogeneous(self) -> Wec3 {
+        Wec3::new(self.x, self.y, f32x4::from(1.0))
     }
 }
 
@@ -537,6 +571,26 @@ impl From<Wec2> for Wec3 {
     }
 }
 
+impl Vec3 {
+    #[inline]
+    pub fn refracted(&mut self, normal: Self, eta: f32) -> Self {
+        let n = normal;
+        let i = *self;
+        let ndi = n.dot(i);
+        let k = 1.0 - eta * eta * (1.0 - ndi * ndi);
+        if k < 0.0 {
+            Self::zero()
+        } else {
+            i * eta - n * (eta * ndi * k.sqrt())
+        }
+    }
+
+    #[inline]
+    pub fn into_homogeneous(self) -> Vec4 {
+        Vec4::new(self.x, self.y, self.z, 1.0)
+    }
+}
+
 impl Wec3 {
     #[inline]
     pub fn splat(vec: Vec3) -> Self {
@@ -550,6 +604,26 @@ impl Wec3 {
             y: f32x4::merge(mask, a.y, b.y),
             z: f32x4::merge(mask, a.z, b.z),
         }
+    }
+
+    #[inline]
+    pub fn refracted(&mut self, normal: Self, eta: f32x4) -> Self {
+        let n = normal;
+        let i = *self;
+        let one = f32x4::from(1.0);
+        let ndi = n.dot(i);
+
+        let k = one - eta * eta * (one - ndi * ndi);
+        let mask = k.cmp_lt(f32x4::from(0.0));
+
+        let out = i * eta - n * (eta * ndi * k.sqrt());
+
+        Self::merge(mask, out, Self::zero())
+    }
+
+    #[inline]
+    pub fn into_homogeneous(self) -> Wec4 {
+        Wec4::new(self.x, self.y, self.z, f32x4::from(1.0))
     }
 }
 
@@ -575,6 +649,377 @@ impl From<[Vec3; 4]> for Wec3 {
             x: f32x4::new(vecs[0].x, vecs[1].x, vecs[2].x, vecs[3].x),
             y: f32x4::new(vecs[0].y, vecs[1].y, vecs[2].y, vecs[3].y),
             z: f32x4::new(vecs[0].z, vecs[1].z, vecs[2].z, vecs[3].z),
+        }
+    }
+}
+
+macro_rules! vec4s {
+    ($($n:ident => $t:ident),+) => {
+        $(#[derive(Clone, Copy, Debug)]
+        pub struct $n {
+            pub x: $t,
+            pub y: $t,
+            pub z: $t,
+            pub w: $t,
+        }
+
+        impl $n {
+            #[inline]
+            pub fn new<T: Into<$t>>(x: T, y: T, z: T, w: T) -> Self {
+                $n { x: x.into(), y: y.into(), z: z.into(), w: w.into() }
+            }
+
+            #[inline]
+            pub fn broadcast<T: Into<$t> + Copy>(val: T) -> Self {
+                Self::new(val, val, val, val)
+            }
+
+            #[inline]
+            pub fn unit_x() -> Self {
+                $n{ x: $t::from(1.0), y: $t::from(0.0), z: $t::from(0.0), w: $t::from(0.0) }
+            }
+
+            #[inline]
+            pub fn unit_y() -> Self {
+                $n{ x: $t::from(0.0), y: $t::from(1.0), z: $t::from(0.0), w: $t::from(0.0) }
+            }
+
+            #[inline]
+            pub fn unit_z() -> Self {
+                $n{ x: $t::from(0.0), y: $t::from(0.0), z: $t::from(1.0), w: $t::from(0.0) }
+            }
+
+            #[inline]
+            pub fn unit_w() -> Self {
+                $n{ x: $t::from(0.0), y: $t::from(0.0), z: $t::from(0.0), w: $t::from(1.0) }
+            }
+
+            #[inline]
+            pub fn dot(&self, other: $n) -> $t {
+                self.x * other.x + self.y * other.y + self.z * other.z + self.w * other.w
+            }
+
+            #[inline]
+            pub fn reflect(&mut self, normal: $n) {
+                *self = *self - ($t::from(2.0) * self.dot(normal) * normal);
+            }
+
+            #[inline]
+            pub fn reflected(&self, normal: $n) -> Self {
+                let mut a = *self;
+                a.reflect(normal);
+                a
+            }
+
+            #[inline]
+            pub fn mag_sq(&self) -> $t {
+                self.x * self.x + self.y * self.y + self.z * self.z + self.w * self.w
+            }
+
+            #[inline]
+            pub fn mag(&self) -> $t {
+                self.mag_sq().sqrt()
+            }
+
+            #[inline]
+            pub fn normalize(&mut self) {
+                let mag = self.mag();
+                self.x /= mag;
+                self.y /= mag;
+                self.z /= mag;
+                self.w /= mag;
+            }
+
+            #[inline]
+            pub fn normalized(&self) -> Self {
+                let mut r = self.clone();
+                r.normalize();
+                r
+            }
+
+            #[inline]
+            pub fn mul_add(&self, mul: $n, add: $n) -> Self {
+                $n::new(
+                    self.x.mul_add(mul.x, add.x),
+                    self.y.mul_add(mul.y, add.y),
+                    self.z.mul_add(mul.z, add.z),
+                    self.w.mul_add(mul.w, add.w),
+                )
+            }
+
+            #[inline]
+            pub fn map<F>(&self, f: F) -> Self
+                where F: Fn($t) -> $t
+            {
+                $n::new(
+                    f(self.x),
+                    f(self.y),
+                    f(self.z),
+                    f(self.w),
+                )
+            }
+
+            #[inline]
+            pub fn apply<F>(&mut self, f: F)
+                where F: Fn($t) -> $t
+            {
+                self.x = f(self.x);
+                self.y = f(self.y);
+                self.z = f(self.z);
+                self.w = f(self.w);
+            }
+
+            #[inline]
+            pub fn component_max(&self) -> $t {
+                self.x.max(self.y).max(self.z).max(self.w)
+            }
+
+            #[inline]
+            pub fn component_min(&self) -> $t {
+                self.x.min(self.y).min(self.z).min(self.w)
+            }
+
+            #[inline]
+            pub fn zero() -> Self {
+                Self::broadcast($t::from(0.0))
+            }
+
+            #[inline]
+            pub fn one() -> Self {
+                Self::broadcast($t::from(1.0))
+            }
+        }
+
+        impl From<[$t; 4]> for $n {
+            #[inline]
+            fn from(comps: [$t; 4]) -> Self {
+                Self::new(comps[0], comps[1], comps[2], comps[3])
+            }
+        }
+
+        impl Add for $n {
+            type Output = Self;
+            #[inline]
+            fn add(self, rhs: $n) -> Self {
+                $n::new(self.x + rhs.x, self.y + rhs.y, self.z + rhs.z, self.w + rhs.w)
+            }
+        }
+
+        impl AddAssign for $n {
+            #[inline]
+            fn add_assign(&mut self, rhs: $n) {
+                self.x += rhs.x;
+                self.y += rhs.y;
+                self.z += rhs.z;
+                self.w += rhs.w;
+            }
+        }
+
+        impl Sub for $n {
+            type Output = Self;
+            #[inline]
+            fn sub(self, rhs: $n) -> Self {
+                $n::new(self.x - rhs.x, self.y - rhs.y, self.z - rhs.z, self.w - rhs.w)
+            }
+        }
+
+        impl SubAssign for $n {
+            #[inline]
+            fn sub_assign(&mut self, rhs: $n) {
+                self.x -= rhs.x;
+                self.y -= rhs.y;
+                self.z -= rhs.z;
+                self.w -= rhs.w;
+            }
+        }
+
+        impl Mul for $n {
+            type Output = Self;
+            #[inline]
+            fn mul(self, rhs: $n) -> Self {
+                $n::new(self.x * rhs.x, self.y * rhs.y, self.z * rhs.z, self.w * rhs. w)
+            }
+        }
+
+        impl Mul<$n> for $t {
+            type Output = $n;
+            #[inline]
+            fn mul(self, rhs: $n) -> $n {
+                $n::new(self * rhs.x, self * rhs.y, self * rhs.z, self * rhs.w)
+            }
+        }
+
+        impl Mul<$t> for $n {
+            type Output = $n;
+            #[inline]
+            fn mul(self, rhs: $t) -> $n {
+                $n::new(self.x * rhs, self.y * rhs, self.z * rhs, self.w * rhs)
+            }
+        }
+
+        impl MulAssign for $n {
+            #[inline]
+            fn mul_assign(&mut self, rhs: $n) {
+                self.x *= rhs.x;
+                self.y *= rhs.y;
+                self.z *= rhs.z;
+                self.w *= rhs.w;
+            }
+        }
+
+        impl MulAssign<$t> for $n {
+            #[inline]
+            fn mul_assign(&mut self, rhs: $t) {
+                self.x *= rhs;
+                self.y *= rhs;
+                self.z *= rhs;
+                self.w *= rhs;
+            }
+        }
+
+        impl Div for $n {
+            type Output = Self;
+            #[inline]
+            fn div(self, rhs: $n) -> Self {
+                $n::new(self.x / rhs.x, self.y / rhs.y, self.z / rhs.z, self.w / rhs.w)
+            }
+        }
+
+        impl Div<$t> for $n {
+            type Output = $n;
+            #[inline]
+            fn div(self, rhs: $t) -> $n {
+                $n::new(self.x / rhs, self.y / rhs, self.z / rhs, self.w / rhs)
+            }
+        }
+
+        impl DivAssign for $n {
+            #[inline]
+            fn div_assign(&mut self, rhs: $n) {
+                self.x /= rhs.x;
+                self.y /= rhs.y;
+                self.z /= rhs.z;
+                self.w /= rhs.w;
+            }
+        }
+
+        impl DivAssign<$t> for $n {
+            #[inline]
+            fn div_assign(&mut self, rhs: $t) {
+                self.x /= rhs;
+                self.y /= rhs;
+                self.z /= rhs;
+                self.w /= rhs;
+            }
+        }
+
+        impl Neg for $n {
+            type Output = $n;
+            #[inline]
+            fn neg(self) -> $n {
+                self * $t::from(-1.0)
+            }
+        })+
+    }
+}
+
+vec4s!(Vec4 => f32, Wec4 => f32x4);
+
+impl From<Vec3> for Vec4 {
+    #[inline]
+    fn from(vec: Vec3) -> Self {
+        Self {
+            x: vec.x,
+            y: vec.y,
+            z: vec.x,
+            w: 0.0,
+        }
+    }
+}
+
+impl From<Wec3> for Wec4 {
+    #[inline]
+    fn from(vec: Wec3) -> Self {
+        Self {
+            x: vec.x,
+            y: vec.y,
+            z: vec.z,
+            w: f32x4::from(0.0),
+        }
+    }
+}
+
+impl Vec4 {
+    #[inline]
+    pub fn refracted(&mut self, normal: Self, eta: f32) -> Self {
+        let n = normal;
+        let i = *self;
+        let ndi = n.dot(i);
+        let k = 1.0 - eta * eta * (1.0 - ndi * ndi);
+        if k < 0.0 {
+            Self::zero()
+        } else {
+            i * eta - n * (eta * ndi * k.sqrt())
+        }
+    }
+}
+
+impl Wec4 {
+    #[inline]
+    pub fn splat(vec: Vec4) -> Self {
+        Self::from([vec, vec, vec, vec])
+    }
+
+    #[inline]
+    pub fn merge(mask: f32x4, a: Self, b: Self) -> Self {
+        Self {
+            x: f32x4::merge(mask, a.x, b.x),
+            y: f32x4::merge(mask, a.y, b.y),
+            z: f32x4::merge(mask, a.z, b.z),
+            w: f32x4::merge(mask, a.w, b.w),
+        }
+    }
+
+    #[inline]
+    pub fn refracted(&mut self, normal: Self, eta: f32x4) -> Self {
+        let n = normal;
+        let i = *self;
+        let one = f32x4::from(1.0);
+        let ndi = n.dot(i);
+
+        let k = one - eta * eta * (one - ndi * ndi);
+        let mask = k.cmp_lt(f32x4::from(0.0));
+
+        let out = i * eta - n * (eta * ndi * k.sqrt());
+
+        Self::merge(mask, out, Self::zero())
+    }
+}
+
+impl Into<[Vec4; 4]> for Wec4 {
+    #[inline]
+    fn into(self) -> [Vec4; 4] {
+        let xs = self.x.as_ref();
+        let ys = self.y.as_ref();
+        let zs = self.z.as_ref();
+        let ws = self.w.as_ref();
+        [
+            Vec4::new(xs[0], ys[0], zs[0], ws[0]),
+            Vec4::new(xs[1], ys[1], zs[1], ws[1]),
+            Vec4::new(xs[2], ys[2], zs[2], ws[2]),
+            Vec4::new(xs[3], ys[3], zs[3], ws[3]),
+        ]
+    }
+}
+
+impl From<[Vec4; 4]> for Wec4 {
+    #[inline]
+    fn from(vecs: [Vec4; 4]) -> Self {
+        Self {
+            x: f32x4::new(vecs[0].x, vecs[1].x, vecs[2].x, vecs[3].x),
+            y: f32x4::new(vecs[0].y, vecs[1].y, vecs[2].y, vecs[3].y),
+            z: f32x4::new(vecs[0].z, vecs[1].z, vecs[2].z, vecs[3].z),
+            w: f32x4::new(vecs[0].w, vecs[1].w, vecs[2].w, vecs[3].w),
         }
     }
 }
