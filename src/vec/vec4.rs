64b4a8fdd7fa86332b23dfc957d4df9f745b8616
Julius Oeftiger <j.oeftiger@protonmail.com>
2021-03-24 09:07:13 +0100

    Generify MulAdd for Vec types

--- src/vec/vec4.rs
+++ src/vec/vec4.rs
@@ -125,16 +125,6 @@ macro_rules! vec4s {
             }
 
             #[inline]
-            pub fn mul_add(&self, mul: $n, add: $n) -> Self {
-                $n::new(
-                    self.x.mul_add(mul.x, add.x),
-                    self.y.mul_add(mul.y, add.y),
-                    self.z.mul_add(mul.z, add.z),
-                    self.w.mul_add(mul.w, add.w),
-                )
-            }
-
-            #[inline]
             pub fn abs(&self) -> Self {
                 Self::new(self.x.abs(), self.y.abs(), self.z.abs(), self.w.abs())
             }
@@ -292,6 +282,34 @@ macro_rules! vec4s {
             #[inline]
             pub fn as_mut_ptr(&mut self) -> *mut $t {
                 self as *mut $n as *mut $t
+            }
+        }
+
+        impl MulAdd for $n {
+            type Output = Self;
+
+            #[inline]
+            fn mul_add(self, a: Self, b: Self) -> Self::Output {
+                $n::new(
+                    self.x.mul_add(a.x, b.x),
+                    self.y.mul_add(a.y, b.y),
+                    self.z.mul_add(a.z, b.z),
+                    self.w.mul_add(a.w, b.w),
+                )
+            }
+        }
+
+        impl MulAdd<$t> for $n {
+            type Output = Self;
+
+            #[inline]
+            fn mul_add(self, a: $t, b: Self) -> Self::Output {
+                $n::new(
+                    self.x.mul_add(a, b.x),
+                    self.y.mul_add(a, b.y),
+                    self.z.mul_add(a, b.z),
+                    self.w.mul_add(a, b.w),
+                )
             }
         }
 
